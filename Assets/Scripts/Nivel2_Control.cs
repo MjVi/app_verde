ba8fe1005f39f6063a72dcc2126019042fa593d2
MjVi <ma.jose.villagomez@gmail.com>
2019-11-27 09:12:21 -0600

    Nivel 1

---
+++ Assets/Scripts/Nivel2_Control.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Nivel2_Control : MonoBehaviour
+{
+    public List<Toggle> abecedario;
+    public List<GameObject> fondos;
+
+    public void Update()
+    {
+        juego();
+
+        if (fondos[0] == true && fondos[1] == true && fondos[2] == true && fondos[3] == true && fondos[4] == true)
+        {
+            Scene_Controller.NextLevel();
+        }
+    }
+
+    public void juego()
+    {
+        if (abecedario[22].isOn == false)
+        {
+            fondos[0].SetActive(true);
+        }
+        if (abecedario[4].isOn == false)
+        {
+            fondos[1].SetActive(true);
+            //if (abecedario[4].isOn == true)
+            //{
+            //    fondos[4].SetActive(true);
+            //}
+        }
+        if (abecedario[18].isOn == false)
+        {
+            fondos[2].SetActive(true);
+        }
+        if (abecedario[3].isOn == false)
+        {
+            fondos[3].SetActive(true);
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/Scene_Controller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Controller : MonoBehaviour
+{
+    public static int level_number=1;
+    public static void NextLevel()
+    {
+        SceneManager.LoadScene("Nivel" + level_number);
+        level_number++;
+    }
+
+    public void Inicio()
+    {
+        SceneManager.LoadScene("Inicio");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
